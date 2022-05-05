9d3b12ae7ab905ed8793d72ad2eebafc92e9474a
SCRATCHER 5-23 <86705803+5-23@users.noreply.github.com>
2022-05-05 22:37:27 +0900

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -2,8 +2,13 @@ let body = document.body
 
 async function main(){
     const req = await fetch("https://url-abcuggi.scratcher5-23.repl.co")
-    var res = await req.json()
-    location.href = res[location.href.replace("https://urlb.tk/" , "")]
+    let res = await req.json()
+    try{
+        console.log(location.href)
+        location.href = res[location.href.replace("https://urlb.tk/" , "")]
+    }catch{
+        body.innerHTML = `404 Not Found`
+    }
 }
 
 main()
