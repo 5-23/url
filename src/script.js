76f68e400d26e3b30444ad90350c14c36ebcd34b
SCRATCHER 5-23 <86705803+5-23@users.noreply.github.com>
2022-05-05 21:46:43 +0900

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,32 +1,9 @@
-// let body = document.body
+let body = document.body
 
-let hrefName = decodeURI(location.href.split("?")[1])
-let res = youtuberJson()
-let html
-
-//https://url-abcuggi.scratcher5-23.repl.co
-async function youtuberJson(){
-    const req = await fetch("https://url-abcuggi.scratcher5-23.repl.co")
-    const res = await req.json()
-    delete req
-    if (req.status == 200){
-        return {
-            status: 200,
-            message: "정상적으로 작동하였습니다",
-            data: res
-        }
-    }else{
-        return {
-            status: res.status,
-            message: "무언가 문제가 있어요",
-            data: {}
-        }
-    }
-}
-    
 async function main(){
-    res = await youtuberJson()
-    console.log(res)
+    const req = await fetch("https://url-abcuggi.scratcher5-23.repl.co")
+    var res = await req.json()
+    location.href = res[location.href.replace("https://urlb.tk/" , "")]
 }
 
 main()
